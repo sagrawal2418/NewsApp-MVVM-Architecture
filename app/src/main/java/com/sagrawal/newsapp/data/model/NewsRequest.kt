2476e7d0f5269092f4b49a5376a198dd47aedd1b
Sandeep Agrawal <sandeepagrawal2418@gmail.com>
2023-12-15 19:43:07 -0500

    remove unused library

--- app/src/main/java/com/sagrawal/newsapp/data/model/NewsRequest.kt
+++ app/src/main/java/com/sagrawal/newsapp/data/model/NewsRequest.kt
@@ -1,11 +1,7 @@
 package com.sagrawal.newsapp.data.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class NewsRequest(
     val news: String? = null,
     val language: String? = null,
     val country: String? = null
-) : Parcelable
+)
