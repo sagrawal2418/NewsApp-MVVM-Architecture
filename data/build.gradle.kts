dc1017341c65cb5437c574e4ba4cf9b2b88f44c1
Sandeep Agrawal <sandeepagrawal2418@gmail.com>
2024-04-08 19:23:13 -0400

    Fixed model and updated build.gradle

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -16,14 +16,14 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     kotlin {
-        jvmToolchain(8)
+        jvmToolchain(17)
     }
 }
 
@@ -33,8 +33,10 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-compiler:2.48")
+    implementation("com.google.dagger:hilt-android:2.49")
+    implementation("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("com.google.dagger:hilt-compiler:2.49")
 
     testImplementation("junit:junit:4.13.2")
 }
